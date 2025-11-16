b5be7b4ae7ffc243fdeceb6084b4dc6786319c70
Igor Pavlić <76666559+igorpavlic@users.noreply.github.com>
2025-11-16 17:15:14 +0100

    JSON korisnici

--- index.js
+++ index.js
@@ -2,6 +2,25 @@ const express = require('express');
 const app = express(); // u varijablu app pohranjujemo objekt koji predstavlja Express aplikaciju
 const PORT = 3000; // port na kojem će poslužitelj slušati zahtjeve
 
+// Polje korisnika
+const korisnici = [
+    {
+        korisnicko_ime: "pero_peric",
+        lozinka: "password123",
+        email: "pperic@gmail.com"
+    },
+    {
+        korisnicko_ime: "ana_anic",
+        lozinka: "ana2024",
+        email: "aanic@gmail.com"
+    },
+    {
+        korisnicko_ime: "marko_maric",
+        lozinka: "marko456",
+        email: "mmaric@gmail.com"
+    }
+];
+
 app.get('/', (req, res) => {
 res.send('Hello, world!');
 });
