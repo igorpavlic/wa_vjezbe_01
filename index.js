9d93c1b576a2b362059af37eced29a5388850534
Igor Pavlić <76666559+igorpavlic@users.noreply.github.com>
2025-11-16 20:16:29 +0100

    Pizze

--- index.js
+++ index.js
@@ -22,6 +22,14 @@ const korisnici = [
     }
 ];
 
+const pizze = [
+{ id: 1, naziv: 'Margherita', cijena: 6.5 },
+{ id: 2, naziv: 'Capricciosa', cijena: 8.0 },
+{ id: 3, naziv: 'Quattro formaggi', cijena: 10.0 },
+{ id: 4, naziv: 'Šunka sir', cijena: 7.0 },
+{ id: 5, naziv: 'Vegetariana', cijena: 9.0 }
+];
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
@@ -34,6 +42,36 @@ app.get('/about', (req, res) => {
 // Nova GET ruta /users
 app.get('/users', (req, res) => {
     res.json(korisnici);
+});
+
+app.get('/pizze', (req, res) => {
+res.json(pizze);
+});
+
+app.get('/pizze/:id', (req, res) => {
+const id_pizza = req.params.id;
+if (isNaN(id_pizza)) {
+    // provjeravamo je li id_pizza "Not a Number"
+    res.json({ message: 'Proslijedili ste parametar id koji nije broj!' });
+    return;
+}
+const pizza = pizze.find(pizza => pizza.id == id_pizza);
+if (pizza) {
+    res.json(pizza);
+} else {
+    res.json({ message: 'Pizza s traženim ID-em ne postoji.' });
+}
+});
+
+app.post('/naruci', (req, res) => {
+const narudzba = req.body;
+const kljucevi = Object.keys(narudzba);
+if (!(kljucevi.includes('pizza') && kljucevi.includes('velicina'))) {
+res.send('Niste poslali sve potrebne podatke za narudžbu!');
+return;
+}
+res.send(`Vaša narudžba za ${narudzba.pizza} (${narudzba.velicina}) je uspješno
+zaprimljena!`);
 });
 
 app.listen(PORT, error => {
